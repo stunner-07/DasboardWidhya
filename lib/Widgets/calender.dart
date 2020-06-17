cb4e6488e1f09daa4663421964ff487901b0a44d
Kshitij Jain <jainkshitij99@gmail.com>
2020-06-17 19:10:14 +0530

    calender basics done

--- lib/Widgets/calender.dart
+++ lib/Widgets/calender.dart
@@ -6,17 +6,52 @@ class Calender extends StatefulWidget {
 }
 
 class _CalenderState extends State<Calender> {
+  String monthInCalender;
+  String yearInCalender;
+  int emptySlots;
+  static const List<String> months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
   int calulateDay(int month, int year) {
+    monthInCalender = months[month - 1];
+    yearInCalender = year.toString();
+    //print(yearInCalender);
     month -= 2;
+    if(month<=0)
+    month--;
     month = (month) % 13;
+    print(month);
     if (month == 11 || month == 12) {
       year--;
     }
     int c = (year / 100).floor();
     int d = (year % 100);
-    return ((1 + ((13 * month - 1) / 5) + d + (d / 4) + (c / 4) - (2 * c))
+    return (((1 +
+                (((13 * month) - 1) / 5).floor() +
+                d +
+                (d / 4).floor() +
+                (c / 4).floor() -
+                (2 * c))
             .floor()) %
-        7;
+        7);
+  }
+
+  @override
+  void didChangeDependencies() {
+    int emp = calulateDay(DateTime.now().month, DateTime.now().year);
+    configureList(emp);
+    super.didChangeDependencies();
   }
 
   List<String> l = [];
@@ -31,6 +66,7 @@ class _CalenderState extends State<Calender> {
     for (int i = 0; i < (4 - empty) % 7; i++) {
       l.add('');
     }
+    emptySlots = empty;
     //setState(() {});
   }
 
@@ -41,17 +77,81 @@ class _CalenderState extends State<Calender> {
       height: 550,
       child: Column(
         children: [
-          RaisedButton(
-            child: Text('Month'),
-            onPressed: () {
-              int emp = calulateDay(DateTime.april, 2020);
-              configureList(emp);
-              setState(() {});
-            },
+          Container(
+            decoration: BoxDecoration(
+                border: Border.all(
+              color: Colors.black,
+              width: 2,
+            )),
+            width: 500,
+            height: 50,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                GestureDetector(
+                  child: Container(
+                    width: 80,
+                    child: Text(monthInCalender + ' ,'),
+                    alignment: Alignment.centerRight,
+                  ),
+                ),
+                GestureDetector(
+                  child: Text(yearInCalender),
+                ),
+                SizedBox(
+                  width: 280,
+                ),
+                IconButton(
+                  alignment: Alignment.center,
+                  onPressed: () {
+                    int m = months.indexOf(monthInCalender);
+                    int y = int.parse(yearInCalender);
+                    if (m == 0) {
+                      y--;
+                      m = 12;
+                    }
+                    int emp = calulateDay(m, y);
+                    configureList(emp);
+                    setState(() {});
+                  },
+                  icon: Icon(Icons.arrow_left),
+                ),
+                IconButton(
+                  onPressed: () {
+                    int m = months.indexOf(monthInCalender);
+                    int y = int.parse(yearInCalender);
+                    if (m == 11) {
+                      y++;
+                      m = -1;
+                    }
+                    int emp = calulateDay(m+2, y);
+                    configureList(emp);
+                    setState(() {});
+                  },
+                  icon: Icon(Icons.arrow_right),
+                ),
+              ],
+            ),
           ),
           Container(
             width: 500,
-            height: calulateDay(DateTime.april, 2020) >= 5 ? 440 : 370,
+            height: 20,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text('Sun'),
+                Text('Mon'),
+                Text('Tue'),
+                Text('Wed'),
+                Text('Thru'),
+                Text('Fri'),
+                Text('Sat'),
+              ],
+            ),
+          ),
+          Container(
+            width: 500,
+            height: emptySlots >= 5 ? 440 : 370,
             decoration: BoxDecoration(
                 border: Border.all(
               color: Colors.black,
